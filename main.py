62c0e40e21df4e2fec7ad2274a0726f751398162
trdburnett <terryronalddavidburnett@gmail.com>
2026-08-17 20:42:57 +0100

    working on display tasks method

--- main.py
+++ main.py
@@ -16,10 +16,18 @@ class Task:
         self.reward = reward
         self.time = time
 
+#adds a task object to the task list
 def add_task(description, priority, reward):
     task = Task(description,priority,reward,time.now())
     tasklist.append(task)
 
+def display_tasks():
+    for task in tasklist:
+        if task.priority == Priority.LOW:
+            print(1)
+
+#hard coded inputs
 add_task("Do 3 sets of bicep curls for 5 reps", "Low", 1)
+
 for task in tasklist:
     print(f"Task: {task.description} ----- Reward: {task.reward} Credits")
