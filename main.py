aac408c9420a9e023ae6aaf10c8385641bf54ed4
trdburnett <terryronalddavidburnett@gmail.com>
2026-08-17 21:12:51 +0100

    -

--- main.py
+++ main.py
@@ -34,5 +34,7 @@ def display_tasks():
 add_task("Do 3 sets of bicep curls for 5 reps", 3, 1)
 add_task("Do 3 lessons on boot.dev", 1, 3)
 add_task("Go for a 10K walk", 2, 2)
+task = Task("Touch grass",1,1,time(2025,1,1))
+tasklist.append(task)
 display_tasks()
 
