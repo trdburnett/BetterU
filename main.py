d6c2b125c179f7b5ffd5a0e93c7998b106c0937e
trdburnett <terryronalddavidburnett@gmail.com>
2026-08-19 14:12:51 +0100

    -

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ parser_add_task.add_argument('description', type=str, help='Description of task'
 parser_add_task.add_argument('priority', type=int, help='Priority of task')
 parser_add_task.add_argument('reward', type=int, help='Reward of task')
 args = parser.parse_args()
-print(args)
+print(args.add_task)
 
 time = datetime.datetime
 tasklist = []
