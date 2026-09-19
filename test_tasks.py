649d855303aa0d02d9c39bdbf45c15af06a6a0aa
trdburnett <terryronalddavidburnett@gmail.com>
2026-09-19 23:09:16 +0100

    added test

--- test_tasks.py
+++ test_tasks.py
@@ -114,7 +114,28 @@ class TestTasks(unittest.TestCase):
                            "tasklist": [],
                            "achievementlist": testachievementlist}
         self.assertEqual(complete_task(testtask_id,testtasklist,testachievementlist,testlast_accessed,testtasks_completed,testhigh_priority_tasks_completed,testmedium_priority_tasks_completed,testlow_priority_tasks_completed,testrewards_claimed,teststreak), expected_result)
-    
+
+    def test_complete_task_success_priority_3_task_in_range_no_achievement_triggered_no_streak_triggered_no_repeat(self):
+        testtask_id = 1
+        testtasklist = [Task("Test Task",3,1,datetime.datetime.now(),1)]
+        testachievementlist = populate_achievement_list([])
+        testlast_accessed = datetime.datetime.now() - datetime.timedelta(hours=1)
+        testtasks_completed = 0
+        testhigh_priority_tasks_completed = 0
+        testmedium_priority_tasks_completed = 0
+        testlow_priority_tasks_completed = 0
+        testrewards_claimed = 0
+        teststreak = 0
+        expected_result = {"credits_to_add": 1,
+                           "tasks_completed_to_add": 1,
+                           "high_priority_tasks_completed_to_add": 0,
+                           "medium_priority_tasks_completed_to_add": 0,
+                           "low_priority_tasks_completed_to_add": 1,
+                           "streak_to_add": 0,
+                           "last_accessed": testlast_accessed,
+                           "tasklist": [],
+                           "achievementlist": testachievementlist}
+        self.assertEqual(complete_task(testtask_id,testtasklist,testachievementlist,testlast_accessed,testtasks_completed,testhigh_priority_tasks_completed,testmedium_priority_tasks_completed,testlow_priority_tasks_completed,testrewards_claimed,teststreak), expected_result)   
 
 if __name__ == "__main__":
     unittest.main()
