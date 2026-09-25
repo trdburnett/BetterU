dbcb9ffce4b714932c7f6eac3869c8838b188337
trdburnett <terryronalddavidburnett@gmail.com>
2026-09-25 21:48:01 +0100

    fixed mistyped import

--- rewards.py
+++ rewards.py
@@ -1,5 +1,5 @@
 from operator import attrgetter
-from display import display_banner, display_padding, display_credits, description_padding, display_top_box, display_box_bottom
+from display import display_banner, display_padding, display_credits, description_padding, display_box_top, display_box_bottom
 
 class Reward:
     def __init__(self,description: str, cost: int, id: int):
@@ -82,7 +82,7 @@ def display_rewards(rewardlist: list, credits: int, terminal_size: set):
         for reward in sorted_rewardlist:
             #print(f"Reward[{reward.id}]: {reward.description}{display_padding(reward.description)}| Cost: {reward.cost} Credits")
             reward_box = ""
-            reward_box = display_top_box(reward.id,"Reward",terminal_size) + "\n"
+            reward_box = display_box_top(reward.id,"Reward",terminal_size) + "\n"
             reward_box = reward_box + description_padding(reward.description,terminal_size) + "\n"
             reward_box = reward_box + description_padding(f"Cost: {reward.cost} Credits",terminal_size) + "\n"
             reward_box = reward_box + display_box_bottom(terminal_size)
